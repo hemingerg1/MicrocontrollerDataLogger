4fa9aef56ffe4ecb0ed0392e28846c01b7e47b98
hemingerg1 <gmheminger1@gmail.com>
2023-10-12 15:53:30 -0400

    embedded software

--- PIO/DataLogger/src/main.cpp
+++ PIO/DataLogger/src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
+#include <Adafruit_ADS1X15.h>
 
-// put function declarations here:
-int myFunction(int, int);
+Adafruit_ADS1115 ads;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+char numSensors = 2;
+int sensorSlopes[] = {625, 625};
+float sensorOffsets[] = {-312.5, -312.5};
+
+String data = "";
+char stream = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  ads.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (stream)
+  {
+    for (int i = 0; i < numSensors; i++)
+    {
+      int16_t adc = ads.readADC_SingleEnded(i);
+      float v = (adc * 0.1875) / 1000;
+      int p = (sensorSlopes[i] * v) + sensorOffsets[i];
+
+      data += p;
+      data += ',';
+    };
+
+    Serial.println(data);
+    data = "";
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void serialEvent()
+{
+  while (Serial.available())
+  {
+    char inChar = Serial.read();
+    if (inChar == '?')
+    {
+      Serial.print('#!#');
+      Serial.print(numSensors);
+      Serial.println('#');
+      stream = 0;
+    }
+    if (inChar == 'A')
+    {
+      stream = 1;
+    }
+    if (inChar == 'S')
+    {
+      stream = 0;
+    }
+  }
 }
