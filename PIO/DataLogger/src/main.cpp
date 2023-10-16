a7f9a3f7c025265566c5c9fbc8de9786a8c99a1d
hemingerg1 <113390240+hemingerg1@users.noreply.github.com>
2023-10-16 16:39:28 -0400

    Updates

--- PIO/DataLogger/src/main.cpp
+++ PIO/DataLogger/src/main.cpp
@@ -1,47 +1,84 @@
 #include <Arduino.h>
+#include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 
 Adafruit_ADS1115 ads;
+#define I2C_SDA 40 // define SCA pin number
+#define I2C_SCL 38 // define SCL pin number
+// Connect ADDR pin to GND for address of 0x48
+TwoWire I2C = TwoWire(0); // create a I2C bus called 'I2C'
 
 int numSensors = 2;
 int sensorSlopes[] = {625, 625};
 float sensorOffsets[] = {-312.5, -312.5};
 
 String data = "";
+int dataSum;
 char stream = 0;
+void serialRead();
 
 void setup()
 {
   Serial.begin(115200);
+  I2C.begin(I2C_SDA, I2C_SCL);
 
+  // Can set gain here not wanting the default setting
   //                                                                ADS1015  ADS1115
   //                                                                -------  -------
   // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
   // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
-  ads.begin();
+
+  ads.begin(0x48, &I2C);
+
+  /*
+  if (!ads.begin(0x48, &I2C))
+  {
+    Serial.println("Failed to initialize ADS.");
+    while (1)
+      ;
+  }
+  */
+
+  pinMode(15, OUTPUT);
 }
 
 void loop()
 {
   if (stream)
   {
+    digitalWrite(15, HIGH); // turn LED on
+    dataSum = 0;
     for (int i = 0; i < numSensors; i++)
     {
-      int16_t adc = ads.readADC_SingleEnded(i);
-      float v = (adc * 0.1875) / 1000;
-      int p = (sensorSlopes[i] * v) + sensorOffsets[i];
+      // int16_t adc = ads.readADC_SingleEnded(i);
+      // float v = (adc * 0.1875) / 1000;
+      int p = 3 + i; //(sensorSlopes[i] * v) + sensorOffsets[i];
 
       data += p;
       data += ',';
+      dataSum += p;
     };
 
+    data += dataSum;
+    data += ',';
     Serial.println(data);
     data = "";
   }
+  else
+  {
+    digitalWrite(15, LOW); // turn LED off
+  }
+
+  delay(1);
+
+  if (Serial.available())
+  {
+    serialRead();
+  }
 }
 
-void serialEvent()
+void serialRead()
 {
   while (Serial.available())
   {
--- PythonUI/Data_Com_ctrl.py
+++ PythonUI/Data_Com_ctrl.py
@@ -77,15 +77,13 @@ class DataMaster():
         temp = self.RowMsg.decode('utf8')
         if len(temp) > 0:
             self.msg = temp.split(",")
-            #del self.msg[0]
-            #if self.msg[0] in "D":
-            #self.messageLen = 0
-            #self.messageLenCheck = 0
             del self.msg[len(self.msg)-1]  # deletes the \n
-            #self.messageLen = int(self.msg[len(self.msg)-1])  # gets the length of the message from the last number
-            #del self.msg[len(self.msg)-1] # deletes the message length from the data
-            #for item in self.msg:
-                #self.messageLenCheck += len(item)  # gets the total length of the data
+            self.messageLen = 0
+            self.messageLenCheck = 0
+            self.messageLen = int(self.msg[len(self.msg)-1])  # gets the sum of the data from the last number
+            del self.msg[len(self.msg)-1] # deletes the sum from the data
+            for item in self.msg:
+                self.messageLenCheck += int(item)  # adds all the data together
             print(self.msg)  ########### DEBUG
 
     def GenChannels(self):
@@ -106,8 +104,11 @@ class DataMaster():
         self.IntMsg = [int(msg) for msg in self.msg]
 
     def StreamDataCheck(self):
-        self.StreamData = True
-        self.IntMsgFunc()
+        self.StreamData = False
+        if self.SynchChannel == len(self.msg):
+            if self.messageLen == self.messageLenCheck:
+                self.StreamData = True
+                self.IntMsgFunc()
               
     def SetRefTime(self):
         if len(self.XData) == 0:
